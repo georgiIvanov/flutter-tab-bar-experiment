a35d32a93fecca9b9f27030997dbf471a93c9267
Georgi Ivanov <georgi.ivanov@tide.co>
2021-08-12 18:53:55 +0300

    move tab bar to separate package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'material_tab_bar.dart';
+import 'package:tab_bar/material_tab_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
