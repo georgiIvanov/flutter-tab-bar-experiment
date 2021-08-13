944241a5d2ed9ec735edfe0576927f916ba81422
Georgi Ivanov <georgi.ivanov@tide.co>
2021-08-13 09:11:21 +0300

    loading icons from provider

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -3,12 +3,33 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tab_bar/material_tab_bar.dart';
 import 'package:tab_bar/tab_bar_notifier.dart';
 
+class IconProviderState {
+  var defaultIcons = <IconData>[];
+  var specialIcons = <IconData>[];
+
+  IconProviderState() {
+    defaultIcons = [Icons.announcement, Icons.alarm, Icons.warning];
+    specialIcons = [
+      Icons.announcement,
+      Icons.alarm,
+      Icons.warning,
+      Icons.airline_seat_flat_angled_rounded
+    ];
+  }
+}
+
+final StateProvider<IconProviderState> iconProvider = StateProvider((ref) {
+  return IconProviderState();
+});
+
 class AppState {}
 
 final StateProvider<AppState> appProvider = StateProvider((ref) {
   final tabBarState = ref.watch(tabBarProvider);
+  final iconState = ref.watch(iconProvider);
 
-  tabBarState.model.icons = [Icons.announcement, Icons.alarm, Icons.warning];
+  // how can we configure this only once
+  tabBarState.model.icons = iconState.state.defaultIcons.toList();
 
   return AppState();
 });
@@ -18,6 +39,8 @@ class AppWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    // watching here is necessary to load icons
+    // but we dont use the watcher in any way. is there another way?
     final watcher = watch(appProvider);
     print(watcher);
 
